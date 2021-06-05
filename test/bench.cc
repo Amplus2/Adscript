3076c379189d54b271b71cf95ea25bcfe2962580
Luddifee <ludwig@ludwig-lehnert.de>
2021-06-05 17:29:55 +0200

    fix bench.cc (2)

--- test/bench.cc
+++ test/bench.cc
@@ -9,18 +9,26 @@ int32_t cxx_fib(int32_t n) {
         return cxx_fib(n - 1) + cxx_fib(n - 2);
 }
 
+using std::chrono::time_point;
+using std::chrono::microseconds;
+using std::chrono::high_resolution_clock;
+
+
 template <class c, class d>
-void print_result(const std::string name, const std::chrono::time_point<c, d> &start, const std::chrono::time_point<c, d> &end) {
-        std::cout << name << " " << std::chrono::microseconds(end - start).count() << std::endl;
+void print_result(const std::string name, const time_point<c, d> &start, const time_point<c, d> &end) {
+        const auto res = end - start;
+        std::cout << name << " " << res.count() << std::endl;
 }
 
 int main() {
-        const auto cxx_start = std::chrono::system_clock::now();
+        const auto cxx_start = high_resolution_clock::now();
         std::cout << cxx_fib(30) << std::endl;
-        const auto cxx_end = std::chrono::system_clock::now();
-        const auto ads_start = std::chrono::system_clock::now();
+        const auto cxx_end = high_resolution_clock::now();
+
+        const auto ads_start = high_resolution_clock::now();
         std::cout << ads_fib(30) << std::endl;
-        const auto ads_end = std::chrono::system_clock::now();
+        const auto ads_end = high_resolution_clock::now();
+
         print_result("Fib C++", cxx_start, cxx_end);
         print_result("Fib Adscript", ads_start, ads_end);
 }
